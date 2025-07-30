bcc66270e15c91c5cbe9730c70ff7ab0a7d9c1a2
HashGame1999 <hashgame1999@gmail.com>
2025-07-30 08:51:51 +0800

    .

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-// use tauri_plugin_sql::{Builder, Migration, MigrationKind};
 
 fn main() {
     ripplewallet_lib::run();
